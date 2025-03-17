4cf30f51e24360ac98c036e3c400cce0bce4c8db
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-03-18 01:30:32 +0300

    feat: practice reference types with a sample

--- value-and-reference-types/script.js
+++ value-and-reference-types/script.js
@@ -24,3 +24,22 @@ console.log("----------------------");
 b = 10; //*the value of "b" changed but it did not affect "a"
 console.log("a : " + a);
 console.log("b : " + b);
+
+//?Reference Types
+
+//----2- Reference data types
+/**
+ * object
+ * array
+ * function
+ */
+
+//*unlike primitive types, reference types affect
+//* each other when assigning values
+let array1 = [1,2,3];
+let array2 = array1;
+
+array2.push(12);//*we added 12 to array2 but it also added to array1
+console.log(array1);
+console.log(array2);//*when we print arrays to the console, "12" will be added to both
+//*so the change we made in array2 will also be seen in array1
