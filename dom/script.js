daf56bcf038134169b2baf3d42b11d0bc71b8adb
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-03-18 02:14:34 +0300

    docs: add comment lines about what DOM is

---
+++ dom/script.js
@@ -0,0 +1,23 @@
+//*What Is The DOM?
+
+//DOM (document object model) is like a map of a web page. When you open a website 
+//the browser creates a structure og the page, and that structure is called the DOM
+
+ /**how does it work
+  * a web page is made of HTML
+  
+  * the browser reads the HTML  and creates a tree like structure
+
+  * this structure lets JavaScript change the page (add,remove or edit elements)
+  */
+
+ //*example
+ //the html document is the tree, the elements (like <h1>,<p>,<button>) are the 
+ //branches and leaves, JS can change or move these branches and leaves. 
+
+ /**things JS can do with DOM;
+  * change text on the page
+  * add or remove elements
+  * change styles (color,size, etc.)
+  * react to user actions (like button clicks)
+  */
