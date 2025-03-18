7c3647cbf6f8930caca03e7b425e3eb37889529b
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-03-19 01:45:16 +0300

    feat: add example and explanation for style features

---
+++ style-feature/script.js
@@ -0,0 +1,6 @@
+//?style feature
+//we can give properties to the element we select, using the style object;
+const todo = document.querySelector(".todo-list");
+
+todo.style.color = "red"; //*we used the style object and gave the element red color,
+//also we can use all css styling properties like backgroundColor, padding, border etc.
