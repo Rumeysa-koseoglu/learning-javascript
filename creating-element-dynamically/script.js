95deb4bad8aec0cd04e575694a05a8a00a1f89b8
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-03-20 02:39:18 +0300

    feat: add example and explanation for creating element dynamically

---
+++ creating-element-dynamically/script.js
@@ -0,0 +1,20 @@
+//?Creating element dynamically
+//*--to create a new element we use the 'createElement' property
+
+const container = document.querySelector(".container");//we assigned the container to a variable
+
+console.log(container);
+
+const link = document.createElement("a");//we created an "a" element by using 'createElement'
+
+link.id = "goBlogWebSite";//with this way we can assign an ID 
+link.className = "btn";//we add "btn" as a class name by using 'className'
+//using cssText we can give the css styles we weote in quotation marks to our element
+link.style.cssText = "background-color: black; margin-top: 20px; padding: 0.5em 0.5em; color: white; border-radius: 5px;"
+link.href = "http://wikipedia.com";//a href address was given 
+link.target = "_blank";//it will open in a new tab
+link.innerHTML = "Kişisel websiteme git" //we wrote text inside the button using 'innerHTML'
+
+container.appendChild(link); //we append this button (link) inside the container div by using 'appendChild'
+
+console.log(link);
