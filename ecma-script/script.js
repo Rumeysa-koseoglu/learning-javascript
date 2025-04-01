8ea6168b3b6455f141a2fb2adef6895bd96b5ffe
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-04-01 16:32:29 +0300

    docs: what is EcmaScript

---
+++ ecma-script/script.js
@@ -0,0 +1,7 @@
+//?Ecma Script
+
+/*ECMAScript is a set of rules, and JavaScript is a language that follows these rules. 
+  Some browsers do not support some js codes but ES  codes , espec'ally ECMAScript 1-6 are fully supported in all modern browsers.
+  Keywords like let, const were included in js with ES6
+  
+  in short , we can write codes supported by every browser with the ES codes included in js*/
