3d1992c06181163cf8f6127b1dc70c397daa6db3
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-04-02 05:25:42 +0300

    feat: add examples for template literals

---
+++ ecma-script/template-literals/scrript.js
@@ -0,0 +1,15 @@
+//?Template Literals
+
+//*old way
+
+ function write(firstName, lastName) {
+
+//console.log("Name : " + firstName + " " + "Last Name : " + lastName )
+
+//*easy way with template literals
+
+console.log(`name : ${firstName} last name : ${lastName}`)
+
+ }
+write("Rumeysa" , "Koseoglu")
+
